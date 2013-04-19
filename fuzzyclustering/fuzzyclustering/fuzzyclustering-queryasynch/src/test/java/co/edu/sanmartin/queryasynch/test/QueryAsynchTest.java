365d8146c855445155b9a7dd31b947c88518e05f
rikardocarvajal <rikardocarvajal@gmail.com>
2013-04-19 17:04:52 +0000

    Adicion de QueryAsynchTest a SVN

---
+++ fuzzyclustering/fuzzyclustering/fuzzyclustering-queryasynch/src/test/java/co/edu/sanmartin/queryasynch/test/QueryAsynchTest.java
@@ -0,0 +1,14 @@
+package co.edu.sanmartin.queryasynch.test;
+import org.junit.Test;
+
+import co.edu.sanmartin.queryasynch.execute.CallRemoteServlet;
+
+
+public class QueryAsynchTest {
+
+	@Test
+	public void sendMessageTest(){
+		CallRemoteServlet call = new CallRemoteServlet();
+		call.sendMessage("Test");
+	}
+}
